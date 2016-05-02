217c213f8118677c611626b1033b33e660f1699f
Harry Lesmana <harry.lesmana@msn.com>
2016-05-02 22:11:08 +0800

    Initial
    - Added app.ini template
    - Streamlining \Raneko\App.
    - Removed unnecessary require in composer.json.

--- src/App.php
+++ src/App.php
@@ -2,379 +2,161 @@
 
 namespace Raneko;
 
-class App
-{
+/**
+ * For required config, please refer to docs/app.ini.placeholder.
+ * @author Harry Lesmana <harry@raneko.com>
+ * @since 2015-11-18
+ */
+class App {
 
-    protected static $_data;
+    const STATIC_KEY_DB_ADAPTER = "zend_db";
+    const STATIC_KEY_DB_ADAPTER_READONLY = "zend_db_ro";
+    const STATIC_KEY_CONFIG = "zend_config";
+    const STATIC_KEY_MONOLOG = "monolog";
 
-    /**
-     * Pointer variable to self::$_data["config"]->config.
-     * @var array
-     */
-    private static $config;
+    private static $data = array();
 
-    const KEY_PHP_DATETIME_UTC_TIMESTAMP = "php_datetime_utc_timestamp";
-    const KEY_ID = "id";
-    const KEY_IP = "ip";
-    const KEY_PORT = "port";
-    const KEY_CONFIG = "config";
-    const KEY_VAR_INC_SERVER = "inc_server";
-    const KEY_VAR_EXC_REQUEST = "exc_server";
-    const KEY_AES = "aes";
-    const KEY_TOKEN = "token";
-    const KEY_HEADERS = "headers";
-
-    /**
-     * Get PHP DateTime representing current time in UTC timezone.
-     * @return \DateTime
-     */
-    static function getDateTimeUTCTimestamp()
-    {
-        isset(self::$_data[self::KEY_PHP_DATETIME_UTC_TIMESTAMP]) || self::$_data[self::KEY_PHP_DATETIME_UTC_TIMESTAMP] = new \DateTime(NULL, new \DateTimeZone("UTC"));
-        return clone self::$_data[self::KEY_PHP_DATETIME_UTC_TIMESTAMP];
+    public static function getFilename($context = "", $extension = "tmp") {
+        strlen($context) == 0 || $context .= "_";
+        return sys_get_temp_dir() . DIRECTORY_SEPARATOR . $context . self::uuid4() . "." . $extension;
     }
 
     /**
-     * Get user ID who invokes the request.
-     * @return int ID of the current user or 0 if user is not recognized. System is recognized as 0.
-     */
-    static function getId()
-    {
-        return isset(self::$_data[self::KEY_ID]) ? self::$_data[self::KEY_ID] : 0;
-    }
-
-    /**
-     * Get IP address who invokes the request.
-     * This method take $_SERVER["REMOTE_ADDR"] by default.
-     * If special parameter is given or system is behind load balancer, IP will be overridden accordingly.
+     * UUID v4.
      * @return string
      */
-    static function getIp()
-    {
-        if (!isset(self::$_data[self::KEY_IP]))
-        {
-            $_ipAddress = $_SERVER["REMOTE_ADDR"];
-            if (isset($_REQUEST["ip"]) && strlen($_REQUEST["ip"]) > 0)
-            {
-                $_ipAddress = $_REQUEST["ip"];
+    public static function uuid4($separator = "") {
+        $data = openssl_random_pseudo_bytes(16);
+
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40); /* set version to 0010 */
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80); /* set bits 6-7 to 10 */
+
+        $result = strtoupper(vsprintf("%s%s{$separator}%s{$separator}%s{$separator}%s{$separator}%s%s%s", str_split(bin2hex($data), 4)));
+
+
+        return $result;
+    }
+
+    public static function rawQuery($query, $dbHost, $dbUser, $dbPass, $dbName) {
+        $mysqli = new \mysqli();
+        $mysqli->init();
+        $mysqli->options(MYSQLI_OPT_LOCAL_INFILE, true);
+        $mysqli->real_connect($dbHost, $dbUser, $dbPass, $dbName);
+        /* check connection */
+        if ($mysqli->connect_errno) {
+            throw new \Exception("Error during DB connection failure:" . $mysqli->error);
+        }
+
+        $result = $mysqli->query($query);
+        $query_result_array = array();
+        $query_result_array['error'] = $mysqli->error;
+        $query_result_array['errorno'] = $mysqli->errno;
+        $mysqli->close();
+        if ($query_result_array['errorno'] <> null && $query_result_array['errorno'] <> "") {
+            throw new \Exception("Error during Query processing:" . $query_result_array['errorno'] . "|Error:" . $query_result_array['error']);
+        }
+        return $query_result_array;
+    }
+
+    /**
+     * Get Zend DB adapter.
+     * @param string $dbHost
+     * @param string $dbUser
+     * @param string $dbPass
+     * @param string $dbName
+     * @param int $dbPort
+     * @return \Zend_Db_Adapter_Abstract Zend DB adapter.
+     */
+    public static function getDbAdapter($dbHost, $dbUser, $dbPass, $dbName, $dbPort = "3306") {
+        $key = self::STATIC_KEY_DB_ADAPTER . md5($dbHost . $dbName . $dbUser);
+        if (!isset(self::$data['getDbAdapter'][$key])) {
+            $config = array(
+                'dbname' => $dbName,
+                'host' => $dbHost,
+                'username' => $dbUser,
+                'password' => $dbPass,
+                'port' => $dbPort,
+                'charset' => "utf8"
+            );
+            self::$data['getDbAdapter'][$key] = new \Zend_Db_Adapter_Pdo_Mysql($config);
+        }
+
+        return self::$data['getDbAdapter'][$key];
+    }
+
+    /**
+     * Set cache data.
+     * @param string $key
+     * @param mixed $data
+     */
+    private static function setData($key, $data) {
+        self::$data[$key] = $data;
+    }
+
+    /**
+     * Get cached data.
+     * @param string $key
+     * @return mixed Cached data.
+     */
+    private static function getData($key) {
+        return isset(self::$data[$key]) ? self::$data[$key] : NULL;
+    }
+
+    /**
+     * Register configuration file.
+     * Only 1 registration is allowed per config name.
+     * @param string $file Absolute path to the config file.
+     * @param string $configName Name to label the config.
+     * @throws \Exception
+     */
+    public static function registerConfigFile($file, $configName = "app") {
+        $key = self::STATIC_KEY_CONFIG . "-{$configName}";
+        $result = self::getData($key);
+        if ($result === NULL) {
+            try {
+                $result = new \Zend_Config_Ini($file);
+                self::addData($key, $result);
+            } catch (\Exception $ex) {
+                throw $ex;
             }
-            elseif (isset($_SERVER["HTTP_X_FORWARDED_FOR"]) && strlen($_SERVER["HTTP_X_FORWARDED_FOR"]) > 0)
-            {
-                $_ipAddress = $_SERVER["HTTP_X_FORWARDED_FOR"];
+        } else {
+            throw new \Exception("Config '{$configName}' is already registered");
+        }
+    }
+
+    /**
+     * Get config object.
+     * @param string $configName Config label.
+     * @return \Zend_Config
+     */
+    public static function getConfig($configName = "app") {
+        $key = self::STATIC_KEY_CONFIG . "-{$configName}";
+        return self::getData($key);
+    }
+
+    /**
+     * Get monolog logger.
+     * @return \Monolog\Logger
+     */
+    public static function getLog($name = "app") {
+        $key = self::STATIC_KEY_MONOLOG . "-" . $name;
+        $result = self::getData($key);
+        if (!isset($result)) {
+            /* Try to get config for path and level */
+            $logPath = sys_get_temp_dir();
+            $logLevel = \Monolog\Logger::DEBUG;
+            $config = self::getConfig();
+            if ($config !== NULL) {
+                !isset($config->app->log->path) || $logPath = $config->app->log->path;
+                !isset($config->app->log->level) || $logLevel = $config->app->log->level;
             }
-            self::setIp($_ipAddress);
+
+            $result = new \Monolog\Logger($name);
+            $handler = new \Monolog\Handler\RotatingFileHandler($logPath . DIRECTORY_SEPARATOR . $name . ".log", 20, $logLevel);
+            $result->pushHandler($handler);
+            self::addData($key, $result);
         }
-
-        return self::$_data[self::KEY_IP];
-    }
-
-    /**
-     * Get IP address who invokes the request in integer.
-     * @return int
-     */
-    static function getIpLong()
-    {
-        return ip2long(self::getIp());
-    }
-
-    static function getHost()
-    {
-        return filter_input(INPUT_SERVER, "HTTP_HOST");
-    }
-
-    /**
-     * Load list of entity from database
-     */
-    private static function loadEntityList()
-    {
-        if (!isset(self::$_data[self::KEY_ENTITY_LIST]))
-        {
-            $_adapter = self::getZendDbAdapter(TRUE);
-            $_dbSelect = $_adapter->select()
-                    ->from("c_entity", array("entity_id" => "id", "entity_name" => "name", "entity_label" => "label"))
-                    ->join("c_entity_field", "c_entity_field.entity_id = c_entity.id", array("field_id" => "id", "field_name" => "name", "field_label" => "label"))
-                    ->join("c_entity_option", "c_entity_option.field_id = c_entity_field.id", array("option_id" => "id", "option_value" => "value", "option_label" => "label"))
-                    ->order("c_entity.id")
-                    ->order("c_entity_field.id")
-                    ->order("c_entity_option.label")
-            ;
-            $_recordList = $_adapter->fetchAll($_dbSelect);
-
-            $_data = array();
-            foreach ($_recordList as $_record)
-            {
-                $_eName = $_record["entity_name"];
-                $_fName = $_record["field_name"];
-                $_oName = $_record["option_value"];
-                if (!isset($_data[$_eName]))
-                {
-                    $_data[$_eName] = array(
-                        "id" => $_record["entity_id"],
-                        "label" => $_record["entity_label"],
-                        "field_list" => array()
-                    );
-                }
-                if (!isset($_data[$_eName]["field_list"][$_fName]))
-                {
-                    $_data[$_eName]["field_list"][$_fName] = array(
-                        "id" => $_record["field_id"],
-                        "label" => $_record["field_label"],
-                        "option_list" => array()
-                    );
-                }
-
-                $_data[$_eName]["field_list"][$_fName]["option_list"][$_oName] = array(
-                    "id" => $_record["option_id"],
-                    "label" => $_record["option_label"]
-                );
-            }
-            self::$_data[self::KEY_ENTITY_LIST] = $_data;
-        }
-    }
-
-    public static function setId($id)
-    {
-        if (!isset(self::$_data[self::KEY_ID]))
-        {
-            self::$_data[self::KEY_ID] = $id;
-        }
-    }
-
-    public static function setIp($ip)
-    {
-        self::$_data[self::KEY_IP] = $ip;
-    }
-
-    public static function setPort($port)
-    {
-        self::$_data[self::KEY_PORT] = $port;
-    }
-
-    /**
-     * Get URI.
-     * @return string
-     */
-    public static function getURI()
-    {
-        return filter_input(INPUT_SERVER, "REQUEST_URI");
-    }
-
-    /**
-     * Configuration object
-     * @param \Raneko\App\Config\ConfigAbstract $file
-     */
-    public static function setConfig($config)
-    {
-        self::$_data[self::KEY_CONFIG] = $config;
-        self::$config = &self::$_data[self::KEY_CONFIG]->config;
-    }
-
-    /**
-     * Get configuration value.
-     * @return mixed
-     * @throw \Exception
-     */
-    public static function getConfig($property = NULL)
-    {
-        if (is_null($property))
-        {
-            return self::$config;
-        }
-
-        $args = func_get_args();
-        if (count($args) == 0)
-        {
-            throw new \Exception("Undefined property");
-        }
-        else
-        {
-            $_root = &self::$config;
-            foreach ($args as $_arg)
-            {
-                if (!isset($_root[$_arg]))
-                {
-                    throw new \Exception("Property '" . implode(".", $args) . "' not found");
-                }
-                $_root = &$_root[$_arg];
-            }
-            return $_root;
-        }
-    }
-
-    /**
-     * Get $_REQUEST variable after filtered.
-     * @return array
-     */
-    public static function getVarRequest()
-    {
-        if (!isset(self::$_data[self::KEY_VAR_EXC_REQUEST]))
-        {
-            $_list = array();
-            try
-            {
-                $_list = self::getConfig("app", "log", "exclusion", "request");
-                if (!is_array($_list))
-                {
-                    $_list = strlen($_list) > 0 ? array($_list) : array();
-                }
-            }
-            catch (Exception $ex)
-            {
-                $_list = FALSE;
-            }
-            self::$_data[self::KEY_VAR_EXC_REQUEST] = $_list;
-        }
-        if (self::$_data[self::KEY_VAR_EXC_REQUEST] === FALSE)
-        {
-            return $_REQUEST;
-        }
-        else
-        {
-            $_result = $_REQUEST;
-            foreach ($_result as $_key => $_value)
-            {
-                foreach (self::$_data[self::KEY_VAR_EXC_REQUEST] as $_field)
-                {
-                    if (stripos($_key, $_field) !== FALSE)
-                    {
-                        $_result[$_key] = str_repeat("*", strlen($_value));
-                    }
-                }
-            }
-            return $_result;
-        }
-    }
-
-    /**
-     * Get $_SERVER after filtered.
-     * @return array
-     */
-    public static function getVarServer()
-    {
-        if (!isset(self::$_data[self::KEY_VAR_INC_SERVER]))
-        {
-            $_list = array();
-            try
-            {
-                $_list = self::getConfig("app", "log", "inclusion", "server");
-                if (!is_array($_list))
-                {
-                    $_list = strlen($_list) > 0 ? array($_list) : array();
-                }
-            }
-            catch (Exception $ex)
-            {
-                $_list = FALSE;
-            }
-            self::$_data[self::KEY_VAR_INC_SERVER] = $_list;
-        }
-        if (self::$_data[self::KEY_VAR_INC_SERVER] === FALSE)
-        {
-            return $_SERVER;
-        }
-        else
-        {
-            $_result = array();
-            foreach (self::$_data[self::KEY_VAR_INC_SERVER] as $_field)
-            {
-                $_field = strtoupper($_field);
-                $_result[$_field] = isset($_SERVER[$_field]) ? $_SERVER[$_field] : NULL;
-            }
-            return $_result;
-        }
-    }
-
-    /**
-     * Get request headers.
-     * There are confusion in regards to different way of getting the headers in PHP which approach might be platform dependent.
-     * This approach is deemed the safest although need to take into consideration that header data might not alway be available.
-     * @return array
-     */
-    public static function getHeaders()
-    {
-        if (!isset(self::$_data[self::KEY_HEADERS]))
-        {
-            $_listHeaders = array();
-            foreach ($_SERVER as $_key => $_value)
-            {
-                if (substr($_key, 0, 5) == "HTTP_")
-                {
-                    $_key = str_replace('_', ' ', substr($_key, 5));
-                    $_key = str_replace(' ', '-', ucwords(strtolower($_key)));
-                    $_listHeaders[$_key] = $_value;
-                }
-            }
-            self::$_data[self::KEY_HEADERS] = $_listHeaders;
-        }
-        return self::$_data[self::KEY_HEADERS];
-    }
-
-    /**
-     * Get default AES handler (using private key from configuration file).
-     * Be very careful when handling private key as it might render data useless.
-     * @return \Raneko\Crypt\AES
-     */
-    public static function getAES()
-    {
-        if (!isset(self::$_data[self::KEY_AES]))
-        {
-            $_aes = new \Raneko\Crypt\AES();
-            $_aes->setKey(self::getConfig("app", "aes", "key"));
-            $_aes->setIV($_aes->generateIV());
-
-            self::$_data[self::KEY_AES] = $_aes;
-        }
-        return self::$_data[self::KEY_AES];
-    }
-
-    /**
-     * Get port currently being used for processing.
-     * Will take $_SERVER["SERVER_PORT"] by default.
-     * Value will adjust if application is behind load balancer.
-     * @return int
-     */
-    public static function getPort()
-    {
-        if (!isset(self::$_data[self::KEY_PORT]))
-        {
-            $_port = $_SERVER["SERVER_PORT"];
-            if (isset($_SERVER["HTTP_X_FORWARDED_PORT"]) && strlen($_SERVER["HTTP_X_FORWARDED_PORT"]) > 0)
-            {
-                $_port = $_SERVER["HTTP_X_FORWARDED_PORT"];
-            }
-            self::setPort($_port);
-        }
-
-        return self::$_data[self::KEY_PORT];
-    }
-
-    /**
-     * Get USER AGENT string.
-     * @return string
-     */
-    public static function getUserAgent()
-    {
-        return $_SERVER["HTTP_USER_AGENT"];
-    }
-
-    /**
-     * Get token associated with the call.
-     * Token must be externally set first via setToken().
-     * @return string|null
-     */
-    public static function getToken()
-    {
-        return isset(self::$_data[self::KEY_TOKEN]) ? self::$_data[self::KEY_TOKEN] : NULL;
-    }
-
-    /**
-     * Set token.
-     * Typically is to be set in bootstrap.
-     * @param string $token
-     */
-    public static function setToken($token)
-    {
-        self::$_data[self::KEY_TOKEN] = $token;
+        return $result;
     }
 
 }
